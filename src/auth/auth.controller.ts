c7fa426ad52aa2a51f576651982f8582e1f3c220
Aiman Muzafar <aimanmuzaffar95@gmail.com>
2026-02-07 20:35:14 +0500

    Move register endpoint in Auth Module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,21 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthEmailLoginDto } from './dtos/auth-email-login.dto';
 import { LoginResponseDto } from './dtos/login-response.dto';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
 
-    constructor(private readonly service: AuthService) {}
+  constructor(private readonly service: AuthService) { }
 
-    @Post('email/login')
-    public login(@Body() loginDto: AuthEmailLoginDto): Promise<LoginResponseDto> {
+  @Post('login')
+  public login(@Body() loginDto: AuthEmailLoginDto): Promise<LoginResponseDto> {
     return this.service.validateLogin(loginDto);
+  }
+
+  @Post('register')
+  public register(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
+    return this.service.register(createUserDto);
   }
 }
